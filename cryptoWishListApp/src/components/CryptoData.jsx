079048d3942c55b788b90189276cc16e5d19b9de
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2024-07-26 18:07:11 +0530

    add simple animation using Framer Motion

--- cryptoWishListApp/src/components/CryptoData.jsx
+++ cryptoWishListApp/src/components/CryptoData.jsx
@@ -1,135 +1,155 @@
 import React, { useState } from "react";
 import {
-	MdKeyboardDoubleArrowLeft,
-	MdKeyboardDoubleArrowRight,
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
 } from "react-icons/md";
+import { AnimatePresence, motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import formatNumber from "../utilities/CurrencyConvert";
 import { Link } from "react-router-dom";
 const tableHeaderData = ["coin", "Price", "24H High", "24H Low"];
 
 const CryptoData = ({ apiData, isHomePage, searchedItem }) => {
-	const [page, setPage] = useState(1);
-	const selectedCurrency = useSelector(
-		(state) => state.CryptoData.selectedCurrency
-	);
-	const itemsPerPage = 10;
-	const filteredData = searchedItem
-		? apiData.filter((data) =>
-				data.name.toLowerCase().includes(searchedItem.toLowerCase())
-		  )
-		: apiData;
+  const [page, setPage] = useState(1);
+  const selectedCurrency = useSelector(
+    (state) => state.CryptoData.selectedCurrency
+  );
+  const itemsPerPage = 10;
+  const filteredData = searchedItem
+    ? apiData.filter((data) =>
+        data.name.toLowerCase().includes(searchedItem.toLowerCase())
+      )
+    : apiData;
 
-	const totalPages = Math.ceil(apiData.length / itemsPerPage);
+  const totalPages = Math.ceil(apiData.length / itemsPerPage);
 
-	const handleNext = () => {
-		setPage((prevPage) => Math.min(prevPage + 1, totalPages));
-	};
+  const handleNext = () => {
+    setPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
 
-	const handlePrevious = () => {
-		setPage((prevPage) => Math.max(prevPage - 1, 1));
-	};
+  const handlePrevious = () => {
+    setPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
 
-	const handlePageClick = (pageNumber) => {
-		setPage(pageNumber);
-	};
+  const handlePageClick = (pageNumber) => {
+    setPage(pageNumber);
+  };
 
-	return (
-		<section className="py-6">
-			<div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full max-w-6xl mx-auto my-4">
-				{isHomePage ? (
-					<h2 className="text-white text-3xl pb-6 font-bold">Top 10 coins</h2>
-				) : (
-					<h2 className="text-white text-3xl pb-6 font-bold">All Coins</h2>
-				)}
+  return (
+    <section className="py-6">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full max-w-6xl mx-auto my-4">
+        {isHomePage ? (
+          <h2 className="text-white text-3xl pb-6 font-bold">Top 10 coins</h2>
+        ) : (
+          <h2 className="text-white text-3xl pb-6 font-bold">All Coins</h2>
+        )}
 
-				<table className="w-full text-sm text-left rtl:text-right text-gray-500  rounded-xl overflow-hidden">
-					<thead className="text-xs text-gray-700 uppercase bg-gray-50 whitespace-nowrap">
-						<tr>
-							{tableHeaderData.map((data) => (
-								<th key={data} scope="col" className="px-6 py-3">
-									<div className="flex items-center">{data}</div>
-								</th>
-							))}
-							<th scope="col" className="px-6 py-3">
-								<span className="sr-only">View More</span>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{filteredData &&
-							filteredData
-								.slice((page - 1) * itemsPerPage, page * itemsPerPage)
-								.map((data) => (
-									<tr key={data.id} className="bg-white border-b ">
-										<th
-											scope="row"
-											className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
-											<div className="flex items-center gap-2">
-												<img src={data.image} alt={data.name} className="w-7" />
-												{data.name}
-											</div>
-										</th>
-										<td className="px-6 py-4">
-											{formatNumber(data.current_price, selectedCurrency)}
-										</td>
-										<td className="px-6 py-4">
-											{formatNumber(data.current_price, selectedCurrency)}
-										</td>
-										<td className="px-6 py-4">
-											{formatNumber(data.current_price, selectedCurrency)}
-										</td>
-										<td className="px-6 py-4 text-right">
-											<Link to={`/chart/${data.id}`}>
-												<button className="font-medium p-2 bg-green-500 text-white whitespace-nowrap">
-													View more
-												</button>
-											</Link>
-										</td>
-									</tr>
-								))}
-					</tbody>
-				</table>
-			</div>
-			{isHomePage ? (
-				""
-			) : (
-				<div className="flex justify-center mt-4">
-					<ul className="inline-flex items-center text-base h-10">
-						<li>
-							<button
-								onClick={handlePrevious}
-								className="flex items-center justify-center px-2 sm:px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 "
-								disabled={page === 1}>
-								<MdKeyboardDoubleArrowLeft />
-							</button>
-						</li>
-						{[...Array(totalPages)].map((_, i) => (
-							<li key={i}>
-								<button
-									onClick={() => handlePageClick(i + 1)}
-									className={`flex items-center justify-center px-2 sm:px-4 h-10 leading-tight ${
-										page === i + 1
-											? "text-white bg-blue-500"
-											: "text-gray-500 bg-white"
-									} border border-gray-300 hover:bg-gray-100 hover:text-gray-700 `}>
-									{i + 1}
-								</button>
-							</li>
-						))}
-						<li>
-							<button
-								onClick={handleNext}
-								className="flex items-center justify-center px-2 sm:px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 "
-								disabled={page === totalPages}>
-								<MdKeyboardDoubleArrowRight />
-							</button>
-						</li>
-					</ul>
-				</div>
-			)}
-		</section>
-	);
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500  rounded-xl overflow-hidden">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 whitespace-nowrap">
+            <tr>
+              {tableHeaderData.map((data) => (
+                <th key={data} scope="col" className="px-6 py-3">
+                  <div className="flex items-center">{data}</div>
+                </th>
+              ))}
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">View More</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <AnimatePresence>
+              {filteredData &&
+                filteredData
+                  .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+                  .map((data, i) => (
+                    <motion.tr
+                      key={data.id}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ delay: i * 0.1 }}
+                      className="bg-white border-b "
+                    >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                      >
+                        <div className="flex items-center gap-2">
+                          <img
+                            src={data.image}
+                            alt={data.name}
+                            className="w-7"
+                          />
+                          {data.name}
+                        </div>
+                      </th>
+                      <td className="px-6 py-4">
+                        {formatNumber(data.current_price, selectedCurrency)}
+                      </td>
+                      <td className="px-6 py-4">
+                        {formatNumber(data.current_price, selectedCurrency)}
+                      </td>
+                      <td className="px-6 py-4">
+                        {formatNumber(data.current_price, selectedCurrency)}
+                      </td>
+                      <td className="px-6 py-4 text-right">
+                        <Link to={`/chart/${data.id}`}>
+                          <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            className="font-medium p-2 bg-green-500 text-white whitespace-nowrap rounded-md"
+                          >
+                            View more
+                          </motion.button>
+                        </Link>
+                      </td>
+                    </motion.tr>
+                  ))}
+            </AnimatePresence>
+          </tbody>
+        </table>
+      </div>
+      {isHomePage ? (
+        ""
+      ) : (
+        <div className="flex justify-center mt-4">
+          <ul className="inline-flex items-center text-base h-10">
+            <li>
+              <button
+                onClick={handlePrevious}
+                className="flex items-center justify-center px-2 sm:px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 "
+                disabled={page === 1}
+              >
+                <MdKeyboardDoubleArrowLeft />
+              </button>
+            </li>
+            {[...Array(totalPages)].map((_, i) => (
+              <li key={i}>
+                <button
+                  onClick={() => handlePageClick(i + 1)}
+                  className={`flex items-center justify-center px-2 sm:px-4 h-10 leading-tight ${
+                    page === i + 1
+                      ? "text-white bg-blue-500"
+                      : "text-gray-500 bg-white"
+                  } border border-gray-300 hover:bg-gray-100 hover:text-gray-700 `}
+                >
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+            <li>
+              <button
+                onClick={handleNext}
+                className="flex items-center justify-center px-2 sm:px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 "
+                disabled={page === totalPages}
+              >
+                <MdKeyboardDoubleArrowRight />
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default CryptoData;
--- cryptoWishListApp/src/components/MarketDetails.jsx
+++ cryptoWishListApp/src/components/MarketDetails.jsx
@@ -3,140 +3,145 @@ import { useSelector, useDispatch } from "react-redux";
 import formatNumber from "../utilities/CurrencyConvert";
 import { MdFavoriteBorder } from "react-icons/md";
 import { toast, ToastContainer } from "react-toastify";
+import { motion } from "framer-motion";
 import {
-	addToWishlist,
-	removeFromWishlist,
+  addToWishlist,
+  removeFromWishlist,
 } from "../features/cryptoData/cryptoDataSlice";
 
 const MarketInfo = ({ label, value }) => (
-	<div className="flex items-center justify-between gap-4 py-2 border-b-2 border-slate-600">
-		<span>{label}</span>
-		<span>{value !== undefined ? value : "N/A"}</span>
-	</div>
+  <div className="flex items-center justify-between gap-4 py-2 border-b-2 border-slate-600">
+    <span>{label}</span>
+    <span>{value !== undefined ? value : "N/A"}</span>
+  </div>
 );
 
 const MarketDetails = ({ apiData }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const { selectedCurrency, wishlist } = useSelector(
-		(state) => state.CryptoData
-	);
-	const wishListedData = wishlist.map((data) => data.id);
-	// console.log(wishListedData);
-	if (!apiData) {
-		return <div>Loading...</div>;
-	}
+  const { selectedCurrency, wishlist } = useSelector(
+    (state) => state.CryptoData
+  );
+  const wishListedData = wishlist.map((data) => data.id);
+  // console.log(wishListedData);
+  if (!apiData) {
+    return <div>Loading...</div>;
+  }
 
-	const { image, name, symbol, market_cap_rank, market_data, id } = apiData;
+  const { image, name, symbol, market_cap_rank, market_data, id } = apiData;
 
-	if (!market_data) {
-		return <div>Market data is unavailable</div>;
-	}
+  if (!market_data) {
+    return <div>Market data is unavailable</div>;
+  }
 
-	const {
-		price_change_percentage_24h: percentageChange,
-		current_price,
-		ath,
-		atl,
-		market_cap,
-		fully_diluted_valuation,
-		circulating_supply,
-		total_supply,
-	} = market_data;
+  const {
+    price_change_percentage_24h: percentageChange,
+    current_price,
+    ath,
+    atl,
+    market_cap,
+    fully_diluted_valuation,
+    circulating_supply,
+    total_supply,
+  } = market_data;
 
-	const priceChangeClass =
-		percentageChange > 0 ? "text-green-600" : "text-red-600";
-	const handleWishList = (id, currentPrice) => {
-		if (id && currentPrice) {
-			dispatch(addToWishlist({ id, currentPrice }));
-			toast.success("Successfully added");
-		}
-	};
-	const handleRemoveWishlist = (id) => {
-		if (id) {
-			dispatch(removeFromWishlist(id));
-			toast.warn("Successfully removed");
-		}
-	};
+  const priceChangeClass =
+    percentageChange > 0 ? "text-green-600" : "text-red-600";
+  const handleWishList = (id, currentPrice) => {
+    if (id && currentPrice) {
+      dispatch(addToWishlist({ id, currentPrice }));
+      toast.success("Successfully added");
+    }
+  };
+  const handleRemoveWishlist = (id) => {
+    if (id) {
+      dispatch(removeFromWishlist(id));
+      toast.warn("Successfully removed");
+    }
+  };
 
-	return (
-		<div className="text-white flex flex-col gap-2 justify-center items-center my-5">
-			<div>
-				<div className="flex gap-2 items-center leading-normal">
-					{image && <img src={image.small} alt={name} />}
-					<span className="text-xl">{name}</span>
-					<span className="uppercase text-xs">{symbol} Price</span>
-					<span className="text-xs bg-gray-600 py-1 px-2 rounded-lg">
-						#{market_cap_rank}
-					</span>
-				</div>
-				<div className="flex gap-2 items-center">
-					<span className="font-bold text-3xl ml-5">
-						{current_price && current_price[selectedCurrency] !== undefined
-							? formatNumber(current_price[selectedCurrency], selectedCurrency)
-							: "N/A"}
-					</span>
-					<span className={priceChangeClass}>
-						{percentageChange !== undefined ? `${percentageChange}%` : "N/A"}
-					</span>
-				</div>
-			</div>
-			<div>
-				{apiData && id ? (
-					!wishListedData.includes(id) ? (
-						<button
-							className="p-2 bg-green-600 rounded-md flex items-center gap-2"
-							onClick={() => handleWishList(id, current_price)}>
-							<MdFavoriteBorder /> Add to WishList
-						</button>
-					) : (
-						<button
-							className="p-2 bg-red-600 rounded-md flex items-center gap-2 "
-							onClick={() => handleRemoveWishlist(id)}>
-							<MdFavoriteBorder /> Remove from WishList
-						</button>
-					)
-				) : (
-					""
-				)}
-			</div>
-			<div className="border-2 border-white flex flex-col gap-2 p-4 w-11/12 md:w-1/2 rounded-xl">
-				<MarketInfo
-					label="All Time High"
-					value={ath && formatNumber(ath[selectedCurrency], selectedCurrency)}
-				/>
-				<MarketInfo
-					label="All Time Low"
-					value={atl && formatNumber(atl[selectedCurrency], selectedCurrency)}
-				/>
-				<MarketInfo
-					label="Market Cap"
-					value={
-						market_cap &&
-						formatNumber(market_cap[selectedCurrency], selectedCurrency)
-					}
-				/>
-				<MarketInfo
-					label="Fully Diluted Valuation"
-					value={
-						fully_diluted_valuation &&
-						formatNumber(
-							fully_diluted_valuation[selectedCurrency],
-							selectedCurrency
-						)
-					}
-				/>
-				<MarketInfo
-					label="Circulating Supply"
-					value={formatNumber(circulating_supply, selectedCurrency, false)}
-				/>
-				<MarketInfo
-					label="Total Supply"
-					value={formatNumber(total_supply, selectedCurrency, false)}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-white flex flex-col gap-2 justify-center items-center my-5">
+      <div>
+        <div className="flex gap-2 items-center leading-normal">
+          {image && <img src={image.small} alt={name} />}
+          <span className="text-xl">{name}</span>
+          <span className="uppercase text-xs">{symbol} Price</span>
+          <span className="text-xs bg-gray-600 py-1 px-2 rounded-lg">
+            #{market_cap_rank}
+          </span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <span className="font-bold text-3xl ml-5">
+            {current_price && current_price[selectedCurrency] !== undefined
+              ? formatNumber(current_price[selectedCurrency], selectedCurrency)
+              : "N/A"}
+          </span>
+          <span className={priceChangeClass}>
+            {percentageChange !== undefined ? `${percentageChange}%` : "N/A"}
+          </span>
+        </div>
+      </div>
+      <div>
+        {apiData && id ? (
+          !wishListedData.includes(id) ? (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className="p-2 bg-green-600 rounded-md flex items-center gap-2"
+              onClick={() => handleWishList(id, current_price)}
+            >
+              <MdFavoriteBorder /> Add to WishList
+            </motion.button>
+          ) : (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              className="p-2 bg-red-600 rounded-md flex items-center gap-2 "
+              onClick={() => handleRemoveWishlist(id)}
+            >
+              <MdFavoriteBorder /> Remove from WishList
+            </motion.button>
+          )
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="border-2 border-white flex flex-col gap-2 p-4 w-11/12 md:w-1/2 rounded-xl">
+        <MarketInfo
+          label="All Time High"
+          value={ath && formatNumber(ath[selectedCurrency], selectedCurrency)}
+        />
+        <MarketInfo
+          label="All Time Low"
+          value={atl && formatNumber(atl[selectedCurrency], selectedCurrency)}
+        />
+        <MarketInfo
+          label="Market Cap"
+          value={
+            market_cap &&
+            formatNumber(market_cap[selectedCurrency], selectedCurrency)
+          }
+        />
+        <MarketInfo
+          label="Fully Diluted Valuation"
+          value={
+            fully_diluted_valuation &&
+            formatNumber(
+              fully_diluted_valuation[selectedCurrency],
+              selectedCurrency
+            )
+          }
+        />
+        <MarketInfo
+          label="Circulating Supply"
+          value={formatNumber(circulating_supply, selectedCurrency, false)}
+        />
+        <MarketInfo
+          label="Total Supply"
+          value={formatNumber(total_supply, selectedCurrency, false)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MarketDetails;
