07f4e5dc2e13e70d677a4b49f00a82aa46b2ba09
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2024-07-27 18:25:18 +0530

    Add loader animation

--- cryptoWishListApp/src/components/Loader.jsx
+++ cryptoWishListApp/src/components/Loader.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import bitcoin from "../assets/bitcoinLoader.png";
 
 const Loader = () => {
-	return (
-		<div className="">
-			<div className="size-28 mx-auto animate-spin-slow">
-				<img src={bitcoin} alt="loaderImage" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-gray-900 h-screen flex justify-center items-center">
+      <div className="size-28 mx-auto animate-spin-slow">
+        <img src={bitcoin} alt="loaderImage" />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
